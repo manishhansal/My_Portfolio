170804df8703a526d798f843bbb882695d425fc4
Manish Kumar <manishhansalmeh@gmail.com>
2022-07-13 20:48:52 +0530

    update

--- src/Components/BelowNavbar/BelowNavbar.jsx
+++ src/Components/BelowNavbar/BelowNavbar.jsx
@@ -53,6 +53,7 @@ const BelowNavbar = () => {
 }
 
 setInterval(function () {
+  
     const show = document.querySelector('span[data-show]')
     const next = show.nextElementSibling || document.querySelector('span:first-child')
     const up = document.querySelector('span[data-up]')
